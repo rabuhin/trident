9e223f5a8a57f81ee20d14d373f4f1e781978f8b
rabuhin <rabuhinvana46@gmail.com>
2026-04-10 16:10:12 +0900

    Main.lua

--- Main.lua
+++ Main.lua
@@ -10,24 +10,24 @@ local ESP = Get("Modules/ESP.lua")
 _G.TridentConfig = Get("Config.lua")
 
 local MainTab = Window:NewTab("Visuals")
-local VisSection = MainTab:NewSection("Player ESP (Radium Style)")
+local VisSection = MainTab:NewSection("Player ESP")
 
-VisSection:NewToggle("Enable ESP", "Отрисовка боксов и линий", function(state)
+VisSection:NewToggle("Enable ESP", "Показать игроков", function(state)
     _G.TridentConfig.ESP_Enabled = state
 end)
 
 local SetTab = Window:NewTab("Settings")
 local SetSection = SetTab:NewSection("Control")
 
-SetSection:NewKeybind("Toggle Menu", "Скрыть меню", Enum.KeyCode.RightControl, function()
-	Library:ToggleUI()
-end)
-
-SetSection:NewButton("Unload Cheat", "Полное удаление скрипта", function()
+SetSection:NewButton("Unload Cheat", "Выход", function()
     if ESP then ESP:Unload() end
     for _, v in pairs(game:GetService("CoreGui"):GetChildren()) do
         if v.Name == "Trident Project | rabuhin" then v:Destroy() end
     end
+end)
+
+SetSection:NewKeybind("Toggle Menu", "Закрыть UI", Enum.KeyCode.RightControl, function()
+	Library:ToggleUI()
 end)
 
 if ESP then
