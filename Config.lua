4bdb184bee8c0632a3a2236ce42bb73179440dc3
rabuhin <rabuhinvana46@gmail.com>
2026-04-10 14:12:28 +0900

    Config.lua

---
+++ Config.lua
@@ -0,0 +1,10 @@
+return {
+    ESP_Enabled = true,
+    ESP_Distance = 2000,
+    Ores_Enabled = true,
+    Colors = {
+        Player = Color3.fromRGB(255, 0, 0),
+        Sulfur = Color3.fromRGB(255, 255, 0),
+        Iron = Color3.fromRGB(150, 150, 150)
+    }
+}
