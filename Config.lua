bc7ca8e0018a8d61e7197924225a95ff878ad149
rabuhin <rabuhinvana46@gmail.com>
2026-04-11 10:59:44 +0900

    Config.lua

--- Config.lua
+++ Config.lua
@@ -1,10 +1,15 @@
 return {
     ESP_Enabled = true,
-    Ores_Enabled = true,
+    Ores_Enabled = false, -- Пока выключим для стабильности
     ESP_Distance = 2500,
     Colors = {
         Player = Color3.fromRGB(255, 80, 80),
         Sulfur = Color3.fromRGB(255, 255, 0),
         Iron = Color3.fromRGB(200, 200, 200)
+    },
+    Settings = {
+        Box_Visible = true,
+        Tracer_Visible = true,
+        Name_Visible = true
     }
 }
